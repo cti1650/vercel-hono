c6b7507fe0501d977f736666c66b81ec7e246f02
cti1650 <cti1650@gmail.com>
2025-02-02 09:07:24 +0000

    feat: 開発用の最低限の環境を整備

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
 import { handle } from 'hono/vercel'
 
 export const config = {
@@ -10,5 +11,17 @@ const app = new Hono().basePath('/api')
 app.get('/', (c) => {
   return c.json({ message: 'Hello Hono!' })
 })
+
+const port = process.env.PORT || 3000;
+
+const main = async () => {
+  console.log(`Server is running on port ${port}`);
+  serve({
+    fetch: app.fetch,
+    port: Number(port),
+  });
+};
+
+main().catch(console.error);
 
 export default handle(app)
