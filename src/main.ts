bbc77bd3d224b538c63f42f984c504344628dc89
cti1650 <cti1650@gmail.com>
2025-02-02 09:25:22 +0000

    feat: add endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { Hono } from 'hono'
 
-export const config = {
-  runtime: 'edge'
-}
-
 export const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
   return c.json({ message: 'Hello Hono!' })
+})
+
+app.get('/health', (ctx) => {
+  return ctx.text('OK')
 })
