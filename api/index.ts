d34c441d2bf6d8d4f1c19bc3628fb8cafd6831a3
cti1650 <cti1650@gmail.com>
2025-02-02 13:59:23 +0000

    fix: update app

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,8 @@
 import { handle } from 'hono/vercel'
 import app from '../src/main'
-import { serveStatic } from '@hono/node-server/serve-static';
 
 export const config = {
-  runtime: 'nodejs'
+  runtime: 'edge'
 }
-
-app.use("/static/*", serveStatic({ root: "./" }));
-app.use("/", serveStatic({ path: "static/index.html" }));
 
 export default handle(app)
--- src/serve.ts
+++ src/serve.ts
@@ -1,13 +1,10 @@
 import { serve } from "@hono/node-server";
 import app from "./main";
-import { serveStatic } from '@hono/node-server/serve-static'
 
 const port = process.env.PORT || 3000;
 
 const main = async () => {
   console.log(`Server is running on http://localhost:${port}`)
-  app.use("/static/*", serveStatic({ root: "./" }));
-  app.use("/", serveStatic({ path: "static/index.html" }));
   serve({
     fetch: app.fetch,
     port: Number(port),
